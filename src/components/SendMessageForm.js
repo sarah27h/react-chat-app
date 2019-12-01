160a44a4b7d8086a7a71290d86d7d1698a9bdf85
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-12-01 22:10:23 +0200

    Feat: Feat: convert <SendMessageForm /> to controlled component

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class SendMessageForm extends Component {
+  state = {
+    message: ''
+  };
+
+  handleChange = e => {
+    this.setState({ message: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.message);
+  };
+
   render() {
     return (
       <div className="send-message-form">
-        <form>
-          <input type="text" placeholder="Type message and hit enter" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.message}
+            onChange={this.handleChange}
+            placeholder="Type message and hit enter"
+          />
         </form>
       </div>
     );
