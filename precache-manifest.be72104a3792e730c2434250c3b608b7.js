79bda4192f6d8cf9492f2a08f10369416a3ba9ef
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-12-11 20:48:14 +0200

    Updates

---
+++ precache-manifest.be72104a3792e730c2434250c3b608b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3695f48ca66cb65f9acbd739657eb6ba",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "9a9b0d34420e8c9cab6a",
+    "url": "/react-chat-app/static/css/main.22dde5cc.chunk.css"
+  },
+  {
+    "revision": "8fc0072b330504c2017c",
+    "url": "/react-chat-app/static/js/2.0166e72d.chunk.js"
+  },
+  {
+    "revision": "9a9b0d34420e8c9cab6a",
+    "url": "/react-chat-app/static/js/main.7ef25dbc.chunk.js"
+  },
+  {
+    "revision": "4e765b30a72205173b22",
+    "url": "/react-chat-app/static/js/runtime-main.196698e0.js"
+  }
+]);
